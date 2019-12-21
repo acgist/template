03d7e50eddb56cf919e596d7b3659b99fc752109
acgist <289547414@qq.com>
2019-12-21 15:18:06 +0800

    数据库

---
+++ acgist-common/acgist-common-data/src/main/java/com/acgist/data/pojo/entity/EntityListener.java
@@ -0,0 +1,34 @@
+package com.acgist.data.pojo.entity;
+
+import java.util.Date;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+/**
+ * <p>Listener - 数据库预处理</p>
+ */
+public class EntityListener {
+
+	/**
+	 * <p>保存预处理</p>
+	 * 
+	 * @param entity 实体
+	 */
+	@PrePersist
+	public void prePersist(BaseEntity entity) {
+		entity.setCreateDate(new Date());
+		entity.setModifyDate(new Date());
+	}
+
+	/**
+	 * <p>更新预处理</p>
+	 * 
+	 * @param entity 实体
+	 */
+	@PreUpdate
+	public void preUpdate(BaseEntity entity) {
+		entity.setModifyDate(new Date());
+	}
+
+}
---
+++ acgist-common/acgist-common-data/src/main/java/com/acgist/utils/ValidatorUtils.java
@@ -0,0 +1,48 @@
+package com.acgist.utils;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.groups.Default;
+
+import com.acgist.core.pojo.Pojo;
+
+/**
+ * <p>utils - 数据校验</p>
+ */
+public class ValidatorUtils {
+
+	private static final Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
+	
+	/**
+	 * <p>校验数据</p>
+	 * 
+	 * @param pojo 数据
+	 * 
+	 * @return 校验结果：{@code null}=成功
+	 */
+	public static final String verify(Pojo pojo) {
+		if(pojo == null) {
+			return null;
+		}
+		final StringBuffer messageBuilder = new StringBuffer();
+		final Set<ConstraintViolation<Pojo>> set = VALIDATOR.validate(pojo, Default.class);
+		if (set != null && !set.isEmpty()) {
+			for (ConstraintViolation<Pojo> violation : set) {
+				messageBuilder
+					.append(violation.getMessage())
+					.append("[")
+					.append(violation.getPropertyPath().toString())
+					.append("]")
+					.append("&");
+			}
+		}
+		if(messageBuilder.length() == 0) {
+			return null;
+		}
+		return messageBuilder.substring(0, messageBuilder.length() - 1);
+	}
+	
+}
