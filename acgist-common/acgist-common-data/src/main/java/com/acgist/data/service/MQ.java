942902a7bfd3b34832df1fa180fc5bc329326110
acgist <289547414@qq.com>
2019-12-26 15:41:21 +0800

    代码优化

---
+++ acgist-common/acgist-common-data/src/main/java/com/acgist/data/service/MQ.java
@@ -0,0 +1,9 @@
+package com.acgist.data.service;
+
+/**
+ * <p>配置重新加载MQ、关机MQ</p>
+ * 配置：权限
+ */
+public class MQ {
+
+}
---
+++ acgist-service/acgist-service/src/main/java/com/acgist/core/service/NotifyService.java
@@ -0,0 +1,13 @@
+package com.acgist.core.service;
+
+/**
+ * <p>service - 网关通知</p>
+ * 
+ * @author acgist
+ * @since 1.0.0
+ * 
+ * TODO：MQ、定时、网关失败不通知
+ */
+public class NotifyService {
+
+}
